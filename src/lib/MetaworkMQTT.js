d97ba2c3ecd650562d39c4602f6bd89c1a992385
LIU SONGTAO <135619114+vettayruu@users.noreply.github.com>
2026-06-11 11:10:38 +0900

    Add files via upload

--- src/lib/MetaworkMQTT.js
+++ src/lib/MetaworkMQTT.js
@@ -2,7 +2,6 @@
 import mqtt from 'mqtt'
 import package_info from '../../package.json' // load version
 import {userUUID} from './cookie_id';
-import { t } from 'numeric';
 
 // export const codeType = package_info.name; // software name
 export const codeType = package_info.customInfo.type; 
@@ -12,12 +11,25 @@ console.log("Robot Type:", codeType);
 // global private variable
 export var mqttclient = null;
 export var idtopic = userUUID;
+export const currentIP = typeof window !== 'undefined' ? window.location.hostname : '';
+console.log("Current IP:", currentIP);
 
 // const MQTT_BROKER_URL = "wss://sora2.uclab.jp/mqws"; // For Nagoya-U UCLab Development
-// const MQTT_BROKER_URL = "wss://192.168.123.51:2118"; // For Local Development, change to your broker address
+// const MQTT_BROKER_URL = "wss://santolina/mqtt"; // For Local Development, change to your broker address
+// const MQTT_BROKER_URL = "wss://133.6.254.50/mqtt"; // For Internet Development, change to your broker address
 const MQTT_BROKER_URL = "wss://liust.local/mqtt";
-// const MQTT_BROKER_URL = "wss://192.168.197.18/mqtt"; // For Local Development, change to your broker address
+
 export var mqttBrokerURL = MQTT_BROKER_URL;
+
+export const MQTT_REGISTER_TOPIC = 'sap/register';
+export const MQTT_UNREGISTER_TOPIC = 'sap/unregister';
+export const MQTT_REQUEST_TOPIC = "sap/request";
+export const MQTT_UNREQUEST_TOPIC = "sap/unrequest";
+export const MQTT_DEVICE_TOPIC = 'sap/dev/'
+export const MQTT_CTRL_TOPIC = 'control/'
+export const MQTT_ROBOT_STATE_TOPIC = 'robot/'
+export const MQTT_ROBOT_SCAN_TOPIC = 'scan/'
+export const MQTT_ROBOT_DATA_TOPIC = 'data/'
 
 export const connectMQTT = (callback) => {
     if (mqttclient == null) {
@@ -30,27 +42,31 @@ export const connectMQTT = (callback) => {
             console.log("MQTT Connected", client);
 
             const date = new Date();
-            var devType = "browser";
-            if(window.location.pathname.endsWith("/viewer/")) {
-                devType = "robot";
-            }
+            // var devType = "browser";
+            // if(window.location.pathname.endsWith("/viewer/")) {
+            //     devType = "robot";
+            // }
             const info = {
                 date: date.toLocaleString(),
-                device: {
-                    //browser: navigator.appName,
-                    //version: navigator.appVersion,
-                    agent: navigator.userAgent,
-                    //platform: navigator.platform,
-                    cookie: navigator.cookieEnabled
-                },
-                devType: devType,
-                codeType: codeType,
+                // device: {
+                //     //browser: navigator.appName,
+                //     //version: navigator.appVersion,
+                //     agent: navigator.userAgent,
+                //     //platform: navigator.platform,
+                //     cookie: navigator.cookieEnabled
+                // },
+                devType: "browser",
+                type: codeType,
                 version: version,
-                devId: userUUID
+                devId: userUUID,
+                optStr: "available"
             }
-            // this is Metawork-MQTT protocol
-            client.publish('mgr/register', JSON.stringify(info)) // for other devices.// maybe retransmit each 10seconds
-            client.subscribe('dev/'+userUUID, {noLocal: true}, (err, granted) => {
+
+            // Register device info to MQTT manager. 
+            client.publish(MQTT_REGISTER_TOPIC, JSON.stringify(info)) 
+
+            // Subscribe to the device topic to receive robotID
+            client.subscribe(MQTT_DEVICE_TOPIC+ userUUID, {noLocal: true}, (err, granted) => {
                 if (!err) {
                     console.log('MQTT Subscribe Granted',  granted);
                 } else {
@@ -61,13 +77,12 @@ export const connectMQTT = (callback) => {
 
         });
         client.on('error', function (err) {
-            console.error('MQTT Connection error: ');
+            console.error('MQTT Connection error: ', err);
         });
         mqttclient = client;
     }
     return mqttclient
 }
-
 
 export const subscribeMQTT = (topic) => {
     if (mqttclient == null) {
